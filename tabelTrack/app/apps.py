9ab12aeec20f610b1abe571032be66542ea9ec32
NurikD <duishoev0123@gmail.com>
2025-06-19 13:03:59 +0500

    manual fix in apps.py after conflict

--- tabelTrack/app/apps.py
+++ tabelTrack/app/apps.py
@@ -1,5 +1,21 @@
 from django.apps import AppConfig
 
+
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+class RequestModelTest(TestCase):
+    def test_invalid_date_range(self):
+        from .models import Request
+        with self.assertRaises(ValidationError):
+            Request.objects.create(
+                user_id=1,
+                type='отпуск',
+                start_date='2025-06-15',
+                end_date='2025-06-10'  # Ошибка: дата окончания раньше начала
+            ).full_clean()
